8954c4c6c230148e5d1ae41d54756f61a3690c2c
Your Name <your-email@example.com>
2025-06-06 17:23:40 +0530

    Rectangle_task

--- Rectangle_task/mywidget.cpp
+++ Rectangle_task/mywidget.cpp
@@ -121,24 +121,3 @@ void MyWidget::paintEvent(QPaintEvent *event)
 
 
 
-// #include "mywidget.h"
-// #include <QPainter>
-// #include <QPaintEvent>
-// #include <QDebug>
-// MyWidget::MyWidget(int x,int y,int width,int height,QWidget *parent)
-//     : QWidget(parent),rectX(x),rectY(y),rectWidth(width),rectheight(height)
-// {
-//     qDebug()<<"Mywidget constructor called";
-// }
-// MyWidget::~MyWidget()
-// {
-//     qDebug()<<"Mywidget destructor called";
-// }
-// void MyWidget::paintEvent(QPaintEvent *event)
-// {
-//     Q_UNUSED(event);
-//     QPainter painter(this);
-//     painter.setPen(Qt::blue);
-//     painter.setBrush(Qt::black);
-//     painter.drawRect(rectX,rectY,rectWidth,rectheight);
-// }
--- Rectangle_task/mywidget.h
+++ Rectangle_task/mywidget.h
@@ -37,24 +37,4 @@ private slots:
 #endif // MYWIDGET_H
 
 
-// #ifndef MYWIDGET_H
-// #define MYWIDGET_H
 
-// #include <QWidget>
-
-// class MyWidget : public QWidget
-// {
-//     Q_OBJECT
-// private:
-//     int rectX;
-//     int rectY;
-//     int rectWidth;
-//     int rectheight;
-// public:
-//      MyWidget(int x,int y,int width,int height,QWidget *paret = nullptr);
-//     ~MyWidget();
-//  protected:
-//     void paintEvent(QPaintEvent *event)override;
-// };
-
-// #endif // MYWIDGET_H

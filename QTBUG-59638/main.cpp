e360fa105bb412b45778c5a92ba091a31dbd4bb4
Your Name <your-email@example.com>
2025-06-17 20:38:59 +0530

    BUG 59638

--- QTBUG-59638/main.cpp
+++ QTBUG-59638/main.cpp
@@ -10,24 +10,47 @@
 //     map.insert("apple", 6);
 //     map.insert("pple", 2);
 //     map.insert("e", 0);
-//     QMultiMap<QString, int> tmap;
-//     tmap.insert("apple", 8);
-//     tmap.insert("bapple", 9);
-//     tmap.insert("zapple", 10);
-//     QMultiMap<int, int> mymap;
-//     mymap.insert(10, 1);
-//     mymap.insert(0, 2);
-//     mymap.insert(10, 10);
-//     QMultiMap<float ,int>qmap;
-//     qmap.insert(10.8,1);
-//     qmap.insert(10.6,2);
-//     qmap.insert(10.9,3);
+//     map.insert("banana",4);
+//     map.insert("pineapple",5);
+//     map.insert("gova",9);
+//     map.insert("mango",9);
+//     map.insert("orange",8);
+//     map.insert("kiwi",3);
+//     map.insert("grapes",7);
+    // QMultiMap<QString, int> tmap;
+    // tmap.insert("apple", 8);
+    // tmap.insert("bapple", 9);
+    // tmap.insert("zapple", 10);
+    // QMultiMap<int, int> mymap;
+    // mymap.insert(10, 1);
+    // mymap.insert(0, 2);
+    // mymap.insert(10, 10);
+    // QMultiMap<float ,int>qmap;
+    // qmap.insert(10.8,1);
+    // qmap.insert(10.6,2);    // QMultiMap<QString, int> tmap;
+    // tmap.insert("apple", 8);
+    // tmap.insert("bapple", 9);
+    // tmap.insert("zapple", 10);
+    // QMultiMap<int, int> mymap;
+    // mymap.insert(10, 1);
+    // mymap.insert(0, 2);
+    // mymap.insert(10, 10);
+    // QMultiMap<float ,int>qmap;
+    // qmap.insert(10.8,1);
+    // qmap.insert(10.6,2);
+    // qmap.insert(10.9,3);
 
-//     qDebug() <<"Valued number : "<<tmap;
+    // qDebug() <<"Valued number : "<<tmap;
 
+    // qDebug() <<"Valued number : "<<map;
+    // qDebug() << "Values for key 'apple':" << mymap;
+    // qmap.insert(10.9,3);
+
+    // qDebug() <<"Valued number : "<<tmap;
+
+    // qDebug() <<"Valued number : "<<map;
+    // qDebug() << "Values for key 'apple':" << mymap;
 //     qDebug() <<"Valued number : "<<map;
-//     qDebug() << "Values for key 'apple':" << mymap;
-//     qDebug() <<"Valued number : "<<qmap;
 
 //     return 0;
 // }
@@ -44,7 +67,6 @@ int main(int argc, char *argv[])
     QMultiHash<QString, int>hash;
     hash.insert("apple", 1);
     hash.insert("apple", 1);
-
     hash.insert("pple", 2);
     hash.insert("e", 0);
     QMultiHash<int ,int>myhash;
